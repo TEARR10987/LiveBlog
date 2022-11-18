7f5804e4623e8813fa4335cedc27aad45aec31d1
CHANGHUNG <117614176+TEARR10987@users.noreply.github.com>
2022-11-18 19:49:06 +0800

    2022/11/18 更新
    新增:驗證碼、帳密驗證、會員登入驗證
    未完成:驗證碼驗證登入

--- Live Blog/LiveBlog.php
+++ Live Blog/LiveBlog.php
@@ -1,3 +1,12 @@
+<?php
+//檢查有沒有名稱是"LoginOK"的Cookie，也檢查值是不是OK，沒有就直接把使用者帶到登入首頁
+if (!isset($_COOKIE["LoginOK"]) || $_COOKIE["LoginOK"] !== "OK") {
+    echo "<h1>這是一個秘密網頁，你不是會員，不能進來</h1>";
+    echo "<a href='index.php'>回到登入首頁！</a>";
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,9 +18,9 @@
     <link rel="stylesheet" href="css/style.css">
     <!-- Box Icons-->
     <link  rel='stylesheet' href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'>
-    
 </head>
 <body>
+    <h2>登入成功</h2>
     <!-- Header -->
     <header>
         <!-- Nav -->
--- Live Blog/login.php
+++ Live Blog/login.php
@@ -6,9 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log in</title>
     <link rel="stylesheet" href="css/login.css">
+    <script>
+        function refresh_code() {
+            document.getElementById("imgcode").src = "captcha.php";
+        }
+    </script>
 </head>
 <body>
     <form action="login_from.php" method="post" >
+        <!-- <div class="back"><button value="Go back!" onclick="history.back()">X</button></div> -->
+        <div class="loginreturn"><input class="back" type="button" value="X" onclick="history.back()"></div>
         <h2>Life Blog</h2>
         <h3>登入即時部落格</h3>
         <h4>沒有任何帳號? <a href="signup.php">立即加入.</a>.</h4>
@@ -16,9 +23,16 @@
         <input class="top" type="text" name="Account" placeholder="帳號">
         <label>密碼</label>
         <input class="top" type="password" name="Password" placeholder="密碼">
+        <label>驗證碼</label>
+        <br>
+        <label class="top"><img id="imgcode" src="captcha.php" onclick="refresh_code()"/></label> 
+        <br>
+        <label class="top" >點擊圖片可以更換驗證碼</label> 
+        <input class="top" type="text" name="Checkword" size="20" maxlength="10" />
         <label>記住我</label>
         <input  type="checkbox" name="RemeberMe" value="YesRememberMe"checked />
-        <button type="submit">登入</button>
+        <button type="submit" >登入</button>
     </form>
+
 </body>
 </html>
